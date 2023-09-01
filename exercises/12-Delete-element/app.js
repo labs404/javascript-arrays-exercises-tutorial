361185f317462f0d87aa2088cb35b874bdb2f7da
labs404 <136141267+labs404@users.noreply.github.com>
2023-09-01 04:17:23 +0000

    fixed merge issue on 12

--- exercises/12-Delete-element/app.js
+++ exercises/12-Delete-element/app.js
@@ -1,6 +1,9 @@
 let people = ['juan','ana','michelle','daniella','stefany','lucy','barak', 'emilio'];
 
 //your code below
+function deletePerson(person) {
+    return people.filter((element) => element != person)
+}
 
 console.log(deletePerson('daniella'));
 console.log(deletePerson('juan'));
